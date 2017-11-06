def3d4d4a6f22617629bb4d422fa251eb72f31cf
GabrielTF141 <gabriel.rato@usp.br>
2017-11-06 18:25:20 -0200

    Search-bar and list items

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+
+
+import {ListGroupItem,ListGroup, Form, FormControl, Button, MenuItem } from 'react-bootstrap';
+
+//TO-DO refactor, is not even a single single bar here...
+function Searchbar() {
+    
+    return(
+        <div>
+        <Form inline>
+            <FormControl
+                className = 'input-search'
+            />
+            <Button bsStyle = 'primary'> x </Button>
+        </Form>
+          <ListGroup>
+            <ListGroupItem>Item 1</ListGroupItem>
+            <ListGroupItem>Item 2</ListGroupItem>
+            <ListGroupItem>...</ListGroupItem>
+          </ListGroup>
+        </div>
+    )
+}
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Searchbar />,
+    document.getElementById('root')
 );
