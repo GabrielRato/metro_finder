3f12c0e44ff0beeaa371584ce3ea694c6c9ecc1b
GabrielTF141 <gabriel.rato@usp.br>
2017-11-07 16:45:21 -0200

    Added items to form

--- src/index.js
+++ src/index.js
@@ -10,27 +10,83 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 import {ListGroupItem,ListGroup, Form, FormControl, Button, MenuItem } from 'react-bootstrap';
 
 //TO-DO refactor, is not even a single single bar here...
-function Searchbar() {
-    
+
+function StationItem(props){
     return(
-        <div>
-        <Form inline>
-            <FormControl
-                className = 'input-search'
-            />
-            <Button bsStyle = 'primary'> x </Button>
-        </Form>
-          <ListGroup>
-            <ListGroupItem>Item 1</ListGroupItem>
-            <ListGroupItem>Item 2</ListGroupItem>
-            <ListGroupItem>...</ListGroupItem>
-          </ListGroup>
-        </div>
-    )
+        <ListGroupItem id={props.id}>
+            { props.value }
+        </ListGroupItem>
+    );
+
 }
 
 
+class MainApp extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleSubmit=this.handleSubmit.bind(this);
+        this.state={
+            //easier to keep childrens sync lift state up to father
+            station: [],
+        };
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const newItem={
+            text: 'item 1',
+            id: Date.now()
+        };
+        console.log(newItem);
+        this.setState(prevState=> ({
+            station: prevState.station.concat(newItem),
+        }));
+    }
+
+    handleClick(){
+        alert('foi');
+    }
+
+    //Using setState redraw is giving for free
+    //Remember react is all about smart state use
+    handleClear(){
+        this.setState({station: [] });
+    }
+
+    render(){
+        return(
+            <div>
+                <Form onSubmit={this.handleSubmit} inline>
+                    <FormControl
+                        className='input-search'
+                    />
+                    <Button bsStyle='primary' onClick={()=> this.handleClick() }> Ok </Button>
+                    <Button bsStyle='primary' onClick={()=> this.handleClear()}> x </Button>
+                </Form>
+                <ListGroup>
+                    <StationList stations={this.state.station} />
+                </ListGroup>
+            </div>
+        )
+    }
+}
+
+class StationList extends React.Component {
+    render() {
+        return(
+            <ul>
+            {this.props.stations.map(item=> (
+                <StationItem key={item.id}
+                    value = {item.text}
+                    id = {item.id}
+                />
+            ))}
+            </ul>
+        );
+    }
+}
+
 ReactDOM.render(
-    <Searchbar />,
+    <MainApp />,
     document.getElementById('root')
 );
